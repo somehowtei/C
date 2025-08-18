d3af8d99ffcad64f99ca542e3ae3cd98c14d6619
teiii <79211121+somehowtei@users.noreply.github.com>
2025-08-18 19:14:12 +0300

    Update fibonacci.c

--- algorithms/fibonacci.c
+++ algorithms/fibonacci.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int main()
+{
+     int n;
+     scanf("%d", &n);
+     int a=1;
+     int b=1;
+     int c;
+     printf("%d %d ", a, b);
+     for(int i=3; i<=n; i++)
+     {
+          c=a+b;
+          a=b;
+          b=c;
+          printf("%d ", c);
+     }
+     return 0;
+}
