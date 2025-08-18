ac271a01743104b8223a664460ef15749a6c19eb
teiii <79211121+somehowtei@users.noreply.github.com>
2025-08-18 18:36:49 +0300

    Create selection_sort.c
    the best sorting program

---
+++ arrays/sort/selection_sort.c
@@ -0,0 +1,24 @@
+int v[100];
+int n;
+scanf("%d", &n)
+for(int i=0; i<n; i++)
+{
+    scanf("%d", &v[i]);
+}
+for(int i=0; i<n; i++)
+{
+    for(int j=i+1; j<n; j++)
+    {
+        if(v[i]>v[j])
+        {
+            v[i]=v[i]^v[j];
+            v[j]=v[i]^v[j];
+            v[i]=v[i]^v[j];
+        }
+    }
+}
+for(int i=0; i<n; i++)
+{
+    printf("%d", v[i]);
+}
+return 0;
