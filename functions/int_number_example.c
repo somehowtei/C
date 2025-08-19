8063c5f8687df63a254d101299a1bd9983de5d0b
teiii <79211121+somehowtei@users.noreply.github.com>
2025-08-19 21:14:55 +0300

    Update int_number_example.c

--- functions/int_number_example.c
+++ functions/int_number_example.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int sumcif(int a)
+{
+  int x;
+  int sum=0;
+  do
+  {
+    x=a%10;
+    sum=sum+x;
+    a=a/10;
+  }while(a>0)
+  return sum;
+}
+int main()
+{
+  int a;
+  int suma=sumcif(a);
+  printf("%d", suma);
+}
