0c77171ec656c764227e5cc70b73cc8baff52b9f
teiii <79211121+somehowtei@users.noreply.github.com>
2025-09-06 16:53:07 +0300

    Update bubble_sort.c

--- arrays/sort/inefficient algorithms/bubble_sort.c
+++ arrays/sort/inefficient algorithms/bubble_sort.c
@@ -13,7 +13,7 @@ do
     {
         if(v[i+1]<v[i])
         {
-            v[i]=v[i]^v[i+1]
+            v[i]=v[i]^v[i+1];
             v[i+1]=v[i]^v[i+1];
             v[i]=v[i]^v[i+1];
             sorted=false;
