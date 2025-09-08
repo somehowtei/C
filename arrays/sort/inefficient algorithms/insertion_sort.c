5cab89c33206ec68987cb289c4defe605c08c114
teiii <79211121+somehowtei@users.noreply.github.com>
2025-09-08 18:04:56 +0300

    Update insertion_sort.c

--- arrays/sort/inefficient algorithms/insertion_sort.c
+++ arrays/sort/inefficient algorithms/insertion_sort.c
@@ -1,21 +1,24 @@
-int v[5] = {3,5,2,1,7};
-
-int n = sizeof(v)/sizeof(int);
-
+int v[1000];
+int n;
 int x;
 int p;
-for (int i=1; i<n;i++)
+scanf("%d", &n);
+for(int i=0; i<n; i++)
+{
+    scanf("%d", &v[i]);
+}
+for (int i=1; i<n; i++)
 {
     x = v[i];
-    p = i-1;
-    while (p>=0 && v[p]>x)
+    p = i;
+    while (p>=1 && v[p-1]>x)
     {
-        v[p+1] = v[p];
+        v[p] = v[p-1];
         p--;
     }
-    v[p+1] = x;
+    v[p] = x;
 }
 for (int i = 0; i < n; i++)
 {
-    printf("%d", v[i]);
+    printf("%d ", v[i]);
 }
