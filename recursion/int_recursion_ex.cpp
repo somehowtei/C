419b286bf3806b09b51adab5cd353f5489728763
teiii <79211121+somehowtei@users.noreply.github.com>
2025-10-07 17:26:44 +0300

    Create int_recursion_ex.cpp

---
+++ recursion/int_recursion_ex.cpp
@@ -0,0 +1,14 @@
+int fact(int n){
+    int r;
+    if(n == 0)
+        r = 1;
+    else
+        r = n * fact(n - 1);
+    return r;
+}
+int main(){
+    int a;
+    a = fact(4);
+    cout << a;
+    return 0;
+}
